c4f2cede8f6e020ec7adaebd4a53a546edd2a671
sachetwasti1997 <sachetwasti61@gmail.com>
2023-08-16 04:51:30 +0530

    first commit

---
+++ src/app/cart/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function CartPage() {
+    return (
+        <div>Cart Page</div>
+    );
+}
+
+export default CartPage;
---
+++ src/app/menu/[category]/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function CategoryPage() {
+    return (
+        <div>Category Page</div>
+    );
+}
+
+export default CategoryPage;
